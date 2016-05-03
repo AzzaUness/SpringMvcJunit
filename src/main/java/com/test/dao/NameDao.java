e83c7cc030a0ef3093125b4fcb267fdfa0598d35
xming <mgabcde@gmail.com>
2016-05-03 18:54:31 +0800

    add packagesToScan property

--- src/main/java/com/test/dao/NameDao.java
+++ src/main/java/com/test/dao/NameDao.java
@@ -2,9 +2,10 @@ package com.test.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import com.test.entity.po.NamePO;
-
+@Repository
 public interface NameDao  extends JpaRepository<NamePO, Long>, JpaSpecificationExecutor<NamePO>{
 
 }
