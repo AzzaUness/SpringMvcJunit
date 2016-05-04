e3df60bf53f3795148ba431dcfa760919e6ce373
xming <mgabcde@gmail.com>
2016-05-04 09:55:26 +0800

    remove persistence.xml

--- src/main/java/com/test/dao/NameDao.java
+++ src/main/java/com/test/dao/NameDao.java
@@ -2,6 +2,7 @@ package com.test.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import com.test.entity.po.NamePO;
